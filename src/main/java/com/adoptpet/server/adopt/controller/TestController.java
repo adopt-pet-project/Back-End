b8e2a09776c1e681dfc06ae6ba51427521f23da4
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-08 23:56:47 +0900

    ⭐ feat: 테스트용 컨트롤러 추가

---
+++ src/main/java/com/adoptpet/server/adopt/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.adoptpet.server.adopt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello project!";
+    }
+}
