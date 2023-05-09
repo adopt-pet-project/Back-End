b0fe4572d88c2a38869af8aa28f39d5e865d93b9
keeptrynjoy <sungmin4218@gmail.com>
2023-05-09 21:15:29 +0900

    🛠️ config: AuditingEntityListener 사용설정

--- src/main/java/com/adoptpet/server/BackEndApplication.java
+++ src/main/java/com/adoptpet/server/BackEndApplication.java
@@ -2,7 +2,9 @@ package com.adoptpet.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //AuditingEntityListener 사용 설정
 @SpringBootApplication
 public class BackEndApplication {
 
