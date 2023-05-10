254590d92673971758e1aac74ebe873961e79c66
keeptrynjoy <sungmin4218@gmail.com>
2023-05-11 01:16:36 +0900

    ⭐ feat: 게시글생성 서비스로직 작성(테스트전)

--- src/main/java/com/adoptpet/server/community/service/CommunityService.java
+++ src/main/java/com/adoptpet/server/community/service/CommunityService.java
@@ -1,11 +1,45 @@
 package com.adoptpet.server.community.service;
 
+import com.adoptpet.server.adopt.repository.MemberRepository;
+import com.adoptpet.server.community.domain.Category;
+import com.adoptpet.server.community.domain.Community;
+import com.adoptpet.server.community.dto.CommunityDto;
+import com.adoptpet.server.community.mapper.CommunityMapper;
+import com.adoptpet.server.community.repository.CategoryRepository;
+import com.adoptpet.server.community.repository.CommunityRepository;
+import com.adoptpet.server.user.domain.Member;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+@Slf4j
 @Service
 @Transactional
-@Slf4j
+@RequiredArgsConstructor
 public class CommunityService {
+
+    private final CommunityRepository communityRepository;
+    private final CategoryRepository categoryRepository;
+    private final MemberRepository memberRepository;
+
+
+    public CommunityDto createArticle(CommunityDto communityDto, String memberEmail, Integer categoryNo){
+
+        final CommunityMapper communityMapper = CommunityMapper.INSTANCE;
+
+        Member findMember = memberRepository.findByEmail(memberEmail)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid member email"));
+
+        Category findCategory = categoryRepository.findById(categoryNo)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category number"));
+
+        Community community = communityMapper.toEntity(communityDto);
+
+        community.addMember(findMember);
+        community.addCategory(findCategory);
+
+        Community saveArticle = communityRepository.save(community);
+
+        return communityMapper.toDTO(saveArticle);
+    }
 }
