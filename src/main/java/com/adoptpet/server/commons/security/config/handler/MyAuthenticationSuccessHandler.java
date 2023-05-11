cdb59531c36e9612c5aa1f0d68048810e4bce8c5
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-11 22:20:11 +0900

    🚀 CI: Deploy & Build yml 수정

--- src/main/java/com/adoptpet/server/commons/security/config/handler/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/adoptpet/server/commons/security/config/handler/MyAuthenticationSuccessHandler.java
@@ -1,68 +1,68 @@
-//package com.adoptpet.server.commons.security.config.handler;
-//
-//import com.adoptpet.server.commons.security.dto.GeneratedToken;
-//import com.adoptpet.server.commons.security.dto.RefreshToken;
-//import com.adoptpet.server.commons.security.dto.SecurityUserDto;
-//import com.adoptpet.server.commons.security.service.JwtUtil;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.util.UriComponentsBuilder;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
-//import java.util.List;
-//
-//@Slf4j
-//@Component
-//@RequiredArgsConstructor
-//public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-//
-//    private final JwtUtil jwtUtil;
-//
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-//        String email = oAuth2User.getAttribute("email");
-//        String provider = oAuth2User.getAttribute("provider");
-//
-//        boolean isExist = oAuth2User.getAttribute("exist");
-//        String role = oAuth2User.getAuthorities().stream().findFirst().get().getAuthority();
-//        log.info("Oauth2UserRole = {}", role);
-//
-//        if (!isExist) {
-//            log.info("토큰 발행 시작");
-//            GeneratedToken token = jwtUtil.generateToken(email, role);
-//            log.info("token = {}", token);
-//
-//
-//           String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/loginCheck")
-//                    .queryParam("accessToken", token.getAccessToken())
-//                    .build()
-//                    .encode(StandardCharsets.UTF_8)
-//                    .toUriString();
-//
-//           getRedirectStrategy().sendRedirect(request, response, targetUrl);
-//
-//
-//        } else {
-//            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/register")
-//                    .queryParam("email", (String) oAuth2User.getAttribute("email"))
-//                    .queryParam("provider", provider)
-//                    .build()
-//                    .encode(StandardCharsets.UTF_8)
-//                    .toUriString();
-//
-//            getRedirectStrategy().sendRedirect(request, response, targetUrl);
-//        }
-//    }
-//
-//}
+package com.adoptpet.server.commons.security.config.handler;
+
+import com.adoptpet.server.commons.security.dto.GeneratedToken;
+import com.adoptpet.server.commons.security.dto.RefreshToken;
+import com.adoptpet.server.commons.security.dto.SecurityUserDto;
+import com.adoptpet.server.commons.security.service.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+        String provider = oAuth2User.getAttribute("provider");
+
+        boolean isExist = oAuth2User.getAttribute("exist");
+        String role = oAuth2User.getAuthorities().stream().findFirst().get().getAuthority();
+        log.info("Oauth2UserRole = {}", role);
+
+        if (!isExist) {
+            log.info("토큰 발행 시작");
+            GeneratedToken token = jwtUtil.generateToken(email, role);
+            log.info("token = {}", token);
+
+
+           String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/loginCheck")
+                    .queryParam("accessToken", token.getAccessToken())
+                    .build()
+                    .encode(StandardCharsets.UTF_8)
+                    .toUriString();
+
+           getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+
+        } else {
+            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/register")
+                    .queryParam("email", (String) oAuth2User.getAttribute("email"))
+                    .queryParam("provider", provider)
+                    .build()
+                    .encode(StandardCharsets.UTF_8)
+                    .toUriString();
+
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        }
+    }
+
+}
--- src/main/java/com/adoptpet/server/commons/security/filter/JwtAuthFilter.java
+++ src/main/java/com/adoptpet/server/commons/security/filter/JwtAuthFilter.java
@@ -1,71 +1,71 @@
-//package com.adoptpet.server.commons.security.filter;
-//
-//import com.adoptpet.server.commons.security.dto.SecurityUserDto;
-//import com.adoptpet.server.commons.security.service.JwtUtil;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.util.List;
-//
-//@RequiredArgsConstructor
-//@Slf4j
-//@Component
-//public class JwtAuthFilter extends OncePerRequestFilter {
-//
-//    private final JwtUtil jwtUtil;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//
-//        if (request.getRequestURI().equals("/token/refresh")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//
-//        String atc = request.getHeader("Authorization");
-//
-//
-//        if (!jwtUtil.verifyToken(atc)) {
-//            log.error("Access Token 만료!");
-//            throw new IllegalStateException("Access Token 만료!");
-//        }
-//
-//        if (atc != null && jwtUtil.verifyToken(atc)) {
-//            String email = jwtUtil.getUid(atc);
-//            String role = jwtUtil.getRole(atc);
-//
-//            // DB연동을 안했으니 이메일 정보로 유저를 만들어주겠습니다
-//            SecurityUserDto userDto = SecurityUserDto.builder()
-//                    .email(email)
-//                    .role(role)
-//                    .name("이름이에용")
-//                    .picture("프로필 이미지에요").build();
-//
-//            log.info("ROLE - ATC = {}", role);
-//
-//            Authentication auth = getAuthentication(userDto);
-//            SecurityContextHolder.getContext().setAuthentication(auth);
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
-//
-//
-//
-//    public Authentication getAuthentication(SecurityUserDto member) {
-//        log.info("ROLE = {}", member.getRole());
-//        return new UsernamePasswordAuthenticationToken(member, "",
-//                List.of(new SimpleGrantedAuthority(member.getRole())));
-//    }
-//
-//}
+package com.adoptpet.server.commons.security.filter;
+
+import com.adoptpet.server.commons.security.dto.SecurityUserDto;
+import com.adoptpet.server.commons.security.service.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getRequestURI().equals("/token/refresh")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String atc = request.getHeader("Authorization");
+
+
+        if (!jwtUtil.verifyToken(atc)) {
+            log.error("Access Token 만료!");
+            throw new IllegalStateException("Access Token 만료!");
+        }
+
+        if (atc != null && jwtUtil.verifyToken(atc)) {
+            String email = jwtUtil.getUid(atc);
+            String role = jwtUtil.getRole(atc);
+
+            // DB연동을 안했으니 이메일 정보로 유저를 만들어주겠습니다
+            SecurityUserDto userDto = SecurityUserDto.builder()
+                    .email(email)
+                    .role(role)
+                    .name("이름이에용")
+                    .picture("프로필 이미지에요").build();
+
+            log.info("ROLE - ATC = {}", role);
+
+            Authentication auth = getAuthentication(userDto);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
+
+    public Authentication getAuthentication(SecurityUserDto member) {
+        log.info("ROLE = {}", member.getRole());
+        return new UsernamePasswordAuthenticationToken(member, "",
+                List.of(new SimpleGrantedAuthority(member.getRole())));
+    }
+
+}
