88f25586084c908aef281fc85954a922aee9fb9b
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-12 21:28:11 +0900

    ⭐ feat: EC2 배포용 URL로 수정

--- src/main/java/com/adoptpet/server/commons/security/config/CustomOAuth2UserService.java
+++ src/main/java/com/adoptpet/server/commons/security/config/CustomOAuth2UserService.java
@@ -1,6 +1,5 @@
 package com.adoptpet.server.commons.security.config;
 
-import com.adoptpet.server.user.repository.MemberRepository;
 import com.adoptpet.server.user.service.MemberService;
 import com.adoptpet.server.user.domain.Member;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
--- src/main/java/com/adoptpet/server/commons/security/config/handler/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/adoptpet/server/commons/security/config/handler/MyAuthenticationSuccessHandler.java
@@ -54,7 +54,7 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
             log.info("jwtToken = {}", token.getAccessToken());
 
             // accessToken을 쿼리스트링에 담는 url을 만들어준다.
-           String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/loginCheck")
+           String targetUrl = UriComponentsBuilder.fromUriString("http://3.36.132.160:3000/loginCheck")
                     .queryParam("accessToken", token.getAccessToken())
                     .build()
                     .encode(StandardCharsets.UTF_8)
@@ -66,7 +66,7 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
         } else {
 
             // 회원이 존재하지 않을경우, 서비스 제공자와 email을 쿼리스트링으로 전달하는 url을 만들어준다.
-            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/register")
+            String targetUrl = UriComponentsBuilder.fromUriString("http://3.36.132.160:3000/register")
                     .queryParam("email", (String) oAuth2User.getAttribute("email"))
                     .queryParam("provider", provider)
                     .build()
