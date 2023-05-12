884c6125ea07a7e80e2339cfa0c3c9b4e03cdd47
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-12 21:47:27 +0900

    🐞 fix: EC2 OAuth 오류 수정

--- src/main/java/com/adoptpet/server/commons/security/config/SecurityConfig.java
+++ src/main/java/com/adoptpet/server/commons/security/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000"); // CORS 허용할 오리진 추가
+        configuration.addAllowedMethod("http://http://ec2-3-36-132-160.ap-northeast-2.compute.amazonaws.com/");
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.addAllowedMethod("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용 설정
