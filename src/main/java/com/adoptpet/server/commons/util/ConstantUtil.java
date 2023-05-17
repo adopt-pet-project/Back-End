531458bd3187b35d4b81aa55fa19a560a1dfd909
keeptrynjoy <sungmin4218@gmail.com>
2023-05-17 20:41:48 +0900

    ⭐ feat: default profile image 상수 추가

--- src/main/java/com/adoptpet/server/commons/util/ConstantUtil.java
+++ src/main/java/com/adoptpet/server/commons/util/ConstantUtil.java
@@ -4,4 +4,7 @@ public abstract class ConstantUtil {
 
     public static final String MASTER_DATASOURCE = "masterDataSource";
     public static final String SLAVE_DATASOURCE = "slaveDataSource";
+
+    public static final String DEFAULT_PROFILE
+            = "https://project-adopt-bucket.s3.ap-northeast-2.amazonaws.com/other/default-profile-image.jpeg";
 }
