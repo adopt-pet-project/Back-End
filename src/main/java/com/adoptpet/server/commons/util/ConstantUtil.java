2e18e176e29d84ee2b4a848b695f684e0e0676d6
keeptrynjoy <sungmin4218@gmail.com>
2023-05-18 11:53:21 +0900

    ⭐ feat: DEFAULT_ADOPT_IMAGE 추가

--- src/main/java/com/adoptpet/server/commons/util/ConstantUtil.java
+++ src/main/java/com/adoptpet/server/commons/util/ConstantUtil.java
@@ -7,4 +7,7 @@ public abstract class ConstantUtil {
 
     public static final String DEFAULT_PROFILE
             = "https://project-adopt-bucket.s3.ap-northeast-2.amazonaws.com/other/default-profile-image.jpeg";
+
+    public static final String DEFAULT_ADOPT_IMAGE
+            ="https://project-adopt-bucket.s3.ap-northeast-2.amazonaws.com/other/cat.jpeg";
 }
