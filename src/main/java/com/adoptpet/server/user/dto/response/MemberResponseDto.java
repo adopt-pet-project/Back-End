a7340bef4395bf345e0a2960e00ef3d30ffe629f
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-19 12:33:31 +0900

    ⭐ feat: 회원 조회 기능 추가 #2

--- src/main/java/com/adoptpet/server/user/dto/response/MemberResponseDto.java
+++ src/main/java/com/adoptpet/server/user/dto/response/MemberResponseDto.java
@@ -1,2 +1,26 @@
-package com.adoptpet.server.user.dto.response;public class MemberResponseDto {
+package com.adoptpet.server.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberResponseDto {
+
+    private Integer id;
+    private String profile;
+    private String name;
+    private String location;
+    private Activity activity;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Activity {
+        private long document;
+        private long comment;
+        private long sanction;
+    }
 }
--- src/main/java/com/adoptpet/server/user/service/MemberQueryService.java
+++ src/main/java/com/adoptpet/server/user/service/MemberQueryService.java
@@ -1,2 +1,65 @@
-package com.adoptpet.server.user.service;public class MemberQueryService {
+package com.adoptpet.server.user.service;
+
+import static com.adoptpet.server.user.domain.QMember.*;
+
+import static com.adoptpet.server.user.domain.QProfileImage.*;
+import static com.adoptpet.server.community.domain.QComment.*;
+import static com.adoptpet.server.community.domain.QCommunity.*;
+import static com.adoptpet.server.user.domain.QMemberDenied.*;
+import com.adoptpet.server.user.dto.response.MemberResponseDto;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberQueryService {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+
+    // 회원의 정보를 조회하는 메서드
+    public MemberResponseDto getUserInfo(Integer memberNo) {
+        return jpaQueryFactory
+                .select(Projections.constructor(MemberResponseDto.class,
+                    member.memberNo,
+                    profileImage.imageUrl,
+                    member.nickname,
+                    member.address,
+                    Projections.constructor(MemberResponseDto.Activity.class,
+                            // 회원 작성 글수 카운팅
+                            ExpressionUtils.as(
+                                    JPAExpressions.select(community.count())
+                                            .from(community)
+                                            .where(community.regId.eq(member.email)),
+                                    "document"
+                            ),
+                            // 회원 작성 댓글수 카운팅
+                            ExpressionUtils.as(
+                                    JPAExpressions.select(comment.count())
+                                            .from(comment)
+                                            .where(comment.member.memberNo.eq(memberNo)),
+                                    "comment"
+                            ),
+                            // 회원 정지 누적 수 카운팅
+                            ExpressionUtils.as(
+                                    JPAExpressions.select(memberDenied.count())
+                                            .from(memberDenied)
+                                            .where(memberDenied.memberNo.eq(memberNo)),
+                                    "sanction"
+                            )
+                    )
+                ))
+                .from(member)
+                .where(member.memberNo.eq(memberNo))
+                .join(profileImage).on(profileImage.memberNo.eq(memberNo))
+                .fetchOne();
+    }
 }
