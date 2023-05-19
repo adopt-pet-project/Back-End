d32b350b95d0089f96f0ffa998957f8f3b926c61
DevSeoRex <ckdekrn88@gmail.com>
2023-05-19 19:27:46 +0900

    🚀 CI: 배포 재시도

--- src/main/java/com/adoptpet/server/adopt/repository/AdoptImageRepository.java
+++ src/main/java/com/adoptpet/server/adopt/repository/AdoptImageRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-
+ 
 
 public interface AdoptImageRepository extends JpaRepository<AdoptImage, Integer> {
 
