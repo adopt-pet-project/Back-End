62608376f16584bfc8cfdda071a210b41f05b80a
DevSeoRex <ckdekrn88@gmail.com>
2023-05-20 00:21:23 +0900

    Update ReportImageRepository.java

--- src/main/java/com/adoptpet/server/user/repository/ReportImageRepository.java
+++ src/main/java/com/adoptpet/server/user/repository/ReportImageRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface ReportImageRepository extends JpaRepository<ReportImage, Integer> {
 
-    @Query("select r from ReportImage r where r.declNo = null")
+    @Query("select rㄱ from ReportImage r where r.declNo = null")
     List<ReportImage> findAllReportImageNull();  
 }
