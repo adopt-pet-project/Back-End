9c89457e9006f5c2591cdc66df8dc52968ce3ac2
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-19 16:48:45 +0900

    🐞 fix: CORS 에러 해결

--- src/main/java/com/adoptpet/server/commons/config/CorsConfig.java
+++ src/main/java/com/adoptpet/server/commons/config/CorsConfig.java
@@ -1,2 +1,18 @@
-package com.adoptpet.server.commons.config;public class CorsConfig {
+package com.adoptpet.server.commons.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+
+    }
 }
