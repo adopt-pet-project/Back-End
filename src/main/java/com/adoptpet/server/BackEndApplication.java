213def046f31d45bf4f6a8a1e975fbdf0086ec11
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-19 19:19:15 +0900

    ⚙️ chore: login redirect url 수정

--- src/main/java/com/adoptpet/server/BackEndApplication.java
+++ src/main/java/com/adoptpet/server/BackEndApplication.java
@@ -3,10 +3,12 @@ package com.adoptpet.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableJpaAuditing //AuditingEntityListener 사용 설정
 @SpringBootApplication
+@EnableScheduling // SpringScheduling 사용 설정
 public class BackEndApplication {
 
     static {
--- src/main/java/com/adoptpet/server/user/repository/ProfileImageRepository.java
+++ src/main/java/com/adoptpet/server/user/repository/ProfileImageRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ProfileImageRepository extends JpaRepository<ProfileImage,Integer> {
 
     @Modifying(clearAutomatically = true)
     @Query("update ProfileImage p set p.memberNo = : memberNo where p.pictureNo = : pictureNo")
     void updateProfileImage(@Param("memberNo") Integer memberNo, @Param("pictureNo") Integer pictureNo);
+
+    @Query("select p from ProfileImage p where p.memberNo = null")
+    List<ProfileImage> findAllProfileImageNull();
 }
