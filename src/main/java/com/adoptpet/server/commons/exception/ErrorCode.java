6b1f02cb6e907edcc3bdc1e80e6a37a55566388f
keeptrynjoy <sungmin4218@gmail.com>
2023-05-22 07:54:05 +0900

    ♻️ refactor: Error 반환 형식 변경

--- src/main/java/com/adoptpet/server/commons/exception/ErrorCode.java
+++ src/main/java/com/adoptpet/server/commons/exception/ErrorCode.java
@@ -11,28 +11,28 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
-    MISSING_REGISTER_BAD_REQUEST(BAD_REQUEST, "등록자에 대한 정보가 입력되지 않았습니다."),
-    MISSING_FILE_BAD_REQUEST(BAD_REQUEST,"요청에 대한 이미지 파일이 누락되었습니다."),
-
+    MISSING_REGISTER_BAD_REQUEST(400, "등록자에 대한 정보가 입력되지 않았습니다."),
+    MISSING_FILE_BAD_REQUEST(400,"요청에 대한 이미지 파일이 누락되었습니다."),
 
     /* 403 FORBIDDEN : 접근 권한 제한 */
     /* Valid : 유효한 */
-    VALID_USER_ID(FORBIDDEN, "해당 정보에 접근 권한이 존재하지 않습니다."),
+    VALID_USER_ID(403, "해당 정보에 접근 권한이 존재하지 않습니다."),
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
-    USER_NOT_FOUND(NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다."),
-    EMAIL_NOT_FOUND(NOT_FOUND, "해당 이메일을 찾을 수 없습니다."),
-    IMAGE_NOT_FOUND(NOT_FOUND,"해당 이미지를 찾을 수 없습니다."),
-    IMAGE_TYPE_NOT_FOUND(NOT_FOUND,"해당 이미지 타입을 찾을 수 없습니다."),
+    USER_NOT_FOUND(404, "해당 유저 정보를 찾을 수 없습니다."),
+    EMAIL_NOT_FOUND(404, "해당 이메일을 찾을 수 없습니다."),
+    IMAGE_NOT_FOUND(404,"해당 이미지를 찾을 수 없습니다."),
+    IMAGE_TYPE_NOT_FOUND(404,"해당 이미지 타입을 찾을 수 없습니다."),
+    ARTICLE_NOT_FOUND(404,"해당 게시글을 찾을 수 없습니다."),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     /* DUPLICATE : (다른 무엇과) 똑같은 */
-    DUPLICATE_EMAIL(CONFLICT, "이메일이 이미 존재합니다."),
+    DUPLICATE_EMAIL(409, "이메일이 이미 존재합니다."),
 
     /* 500 : */
-    UNSUCCESSFUL_UPLOAD(INTERNAL_SERVER_ERROR,"이미지 파일 업로드에 실패했습니다.");
+    UNSUCCESSFUL_UPLOAD(500,"이미지 파일 업로드에 실패했습니다.");
 
-    private final HttpStatus httpStatus;
+    private final Integer status;
     private final String massage;
 
     public static CustomException throwNotEnterEmailOrToken(){
@@ -49,5 +49,9 @@ public enum ErrorCode {
 
     public static CustomException throwImageTypeNotFound() {
         throw new CustomException(IMAGE_TYPE_NOT_FOUND);
+    }
+
+    public static CustomException throwArticleNotFound() {
+        throw new CustomException(ARTICLE_NOT_FOUND);
     }
 }
