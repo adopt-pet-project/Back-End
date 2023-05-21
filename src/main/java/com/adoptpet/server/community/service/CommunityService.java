4496684630547c0d3cd60e454395a361ff76fdef
keeptrynjoy <sungmin4218@gmail.com>
2023-05-22 08:53:34 +0900

    ♻️ refactor: Category 검증 로직 수정

--- src/main/java/com/adoptpet/server/community/service/CommunityService.java
+++ src/main/java/com/adoptpet/server/community/service/CommunityService.java
@@ -16,11 +16,9 @@ import com.adoptpet.server.community.service.mapper.CreateArticleMapper;
 import com.adoptpet.server.community.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Objects;
@@ -37,23 +35,44 @@ public class CommunityService {
     private final CommunityImageRepository communityImageRepository;
     private final CategoryRepository categoryRepository;
 
-    private Community findByArticleNo(Integer articleNo){
+    //== 게시글 유무 검증 ==//
+    @Transactional(readOnly = true)
+    public Community findByArticleNo(Integer articleNo){
         // 게시글 번호로 게시글을 조회하고, 조회되지 않을 경우 예외를 발생시킨다.
-        return communityRepository.findById(articleNo)
-                .orElseThrow(ErrorCode::throwArticleNotFound);
+        Optional<Community> findCommunity = communityRepository.findById(articleNo);
+        if(!findCommunity.isPresent()){
+            throw new CustomException(ErrorCode.ARTICLE_NOT_FOUND);
+        }
+        return findCommunity.get();
+    }
+
+    //== 카테고리 유무 검증 ==//
+    @Transactional(readOnly = true)
+    public void checkCategoryNo(Integer categoryNo) {
+        Optional<Category> findCategory = categoryRepository.findById(categoryNo);
+        if(!findCategory.isPresent()){
+            throw new CustomException(ErrorCode.CATEGORY_NOT_FOUND);
+        }
     }
 
     /**
     * 게시글 수정
     **/
+    @Transactional
     public Community updateArticle(CommunityDto communityDto, Integer articleNo){
 
         // 게시글 고유키로 게시글 검증
         Community community = findByArticleNo(articleNo);
+
         // 게시글 데이터 엔티티 저장
         community.updateArticleByMod(communityDto, SecurityUtils.getUser().getEmail());
+
+        // 카테고리 번호 검증
+        checkCategoryNo(community.getCategoryNo());
+
         // 이미지 업데이트
         updateImageByArticleNo(communityDto.getImage(),articleNo);
+
         // 게시글 업데이트
         Community updatedArticle = communityRepository.save(community);
 
@@ -68,21 +87,27 @@ public class CommunityService {
     public ArticleDetailInfo readArticle(Integer articleNo, String accessToken){
         // 게시글 고유키로 게시글 검증
         findByArticleNo(articleNo);
+
         // 게시글 정보 조회
         ArticleDetailInfo articleDetail = communityQDslRepository.findArticleDetail(articleNo);
+
         // 조회 유저 검증 기본 값 지정
         articleDetail.addIsMine(false);
+
         // 엑세스 토큰 있을 시 조회한 유저가 게시글의 주인인지 확인
         if (StringUtils.hasText(accessToken)) {
             // 현재 게시글을 보려는 회원이 이 게시글을 작성한 작성자와 같은지 확인한다.
             boolean isMine = communityQDslRepository.isMine(SecurityUtils.getUser().getEmail(), articleNo);
             articleDetail.addIsMine(true);
         }
+
         // 이미지 URL 조회 -> 없을 시 null 반환
         List<String> images = communityImageRepository.findImageUrlByArticleNo(articleNo)
                     .orElse(null);
+
         // 이미지 URL 리스트 추가
         articleDetail.addImages(images);
+
         // 조회된 게시글 정보 반환
         return articleDetail;
     }
@@ -97,26 +122,30 @@ public class CommunityService {
     public CommunityDto insertArticle(CommunityDto communityDto){
         // CreateArticleMapper 인스턴스 생성
         final CreateArticleMapper createArticleMapper = CreateArticleMapper.INSTANCE;
+
         // DTO를 Entity로 매핑
         Community community = createArticleMapper.toEntity(communityDto);
 
-        // 카테고리 번호 확인
-        Optional<Category> findCategory = categoryRepository.findById(community.getCategoryNo());
-        if(!findCategory.isPresent()){
-            throw new CustomException(ErrorCode.CATEGORY_NOT_FOUND);
-        }
+        // 카테고리 번호 검증
+        checkCategoryNo(community.getCategoryNo());
 
-        // Community 저장
+        // Community DB 저장
         Community saveArticle = communityRepository.save(community);
+
         // 이미지 업데이트
         ArticleImageDto[] images = communityDto.getImage();
         updateImageByArticleNo(communityDto.getImage(), saveArticle.getArticleNo());
+
         // 저장된 Community를 DTO로 변환
         CommunityDto response = createArticleMapper.toDTO(saveArticle);
+
         // 반환값에 null 대신 image 기입
         response.addImgNo(images);
+
         return response;
     }
+
+
 
 
     @Transactional
