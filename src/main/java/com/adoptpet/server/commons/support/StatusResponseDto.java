86886029d2646972b29453bb32caa7b0f4c2af25
keeptrynjoy <sungmin4218@gmail.com>
2023-05-22 06:49:37 +0900

    ⭐ feat: success 메서드 추가

--- src/main/java/com/adoptpet/server/commons/support/StatusResponseDto.java
+++ src/main/java/com/adoptpet/server/commons/support/StatusResponseDto.java
@@ -1,16 +1,28 @@
 package com.adoptpet.server.commons.support;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // DTO 를 JSON으로 변환 시 null값인 field 제외
 public class StatusResponseDto {
     private Integer status;
+    private String massage;
 
-
+    public StatusResponseDto(Integer status) {
+        this.status = status;
+    }
 
     public static StatusResponseDto addStatus(Integer status) {
         return new StatusResponseDto(status);
+    }
+
+    public static StatusResponseDto success(){
+        return new StatusResponseDto(200);
+    }
+    public static StatusResponseDto success(String massage){
+        return new StatusResponseDto(200, massage);
     }
 }
