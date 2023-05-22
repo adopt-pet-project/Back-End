09d2fdb033695620159e047b64ed7e54803ada52
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-22 17:28:47 +0900

    ⭐ feat: Kafka Stomp 셋팅

--- src/main/java/com/adoptpet/server/adopt/dto/chat/Message.java
+++ src/main/java/com/adoptpet/server/adopt/dto/chat/Message.java
@@ -1,2 +1,28 @@
-package com.adoptpet.server.adopt.dto.chat;public class Messate {
+package com.adoptpet.server.adopt.dto.chat;
+
+
+import com.adoptpet.server.adopt.domain.mongo.Chatting;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Message implements Serializable {
+
+    private Integer chatNo;
+    private String contentType;
+    private String content;
+
+    private LocalDateTime sendTime;
+
+    public void setSendTime(LocalDateTime sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public Chatting convertEntity() {
+        return Chatting.builder()
+                .chatRoomNo(chatNo)
+                .content(contentType)
+                .content(content)
+                .build();
+    }
 }
--- src/main/java/com/adoptpet/server/commons/config/interceptor/StompHandler.java
+++ src/main/java/com/adoptpet/server/commons/config/interceptor/StompHandler.java
@@ -1,2 +1,60 @@
-package com.adoptpet.server.commons.config.interceptor;public class StompHandler {
+package com.adoptpet.server.commons.config.interceptor;
+
+import com.adoptpet.server.commons.security.dto.SecurityUserDto;
+import com.adoptpet.server.commons.security.service.JwtUtil;
+import com.adoptpet.server.user.domain.Member;
+import com.adoptpet.server.user.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@Component
+@RequiredArgsConstructor
+public class StompHandler implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if (accessor.getCommand() == StompCommand.CONNECT) {
+            String accessToken = accessor.getFirstNativeHeader("Authorization");
+            if (!jwtUtil.verifyToken(accessToken)) {
+                throw new IllegalStateException("토큰이 만료되었습니다.");
+            } else {
+                accessor.setUser(getAuthentication(accessToken));
+            }
+        }
+
+        return message;
+    }
+
+    private Authentication getAuthentication(String accessToken) {
+        // AccessToken 내부의 payload에 있는 email로 user를 조회한다. 없다면 예외를 발생시킨다 -> 정상 케이스가 아님
+        Member findMember = memberRepository.findByEmail(jwtUtil.getUid(accessToken))
+                .orElseThrow(IllegalStateException::new);
+
+        // SecurityContext에 등록할 User 객체를 만들어준다.
+        SecurityUserDto userDto = SecurityUserDto.builder()
+                .memberNo(findMember.getMemberNo())
+                .email(findMember.getEmail())
+                .role("ROLE_".concat(findMember.getUserRole()))
+                .nickname(findMember.getNickname())
+                .build();
+
+        return new UsernamePasswordAuthenticationToken(userDto, "",
+                List.of(new SimpleGrantedAuthority(userDto.getRole())));
+    }
 }
