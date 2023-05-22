364691774ebbb0e8a95ba09b3080fbc265ec1a4c
keeptrynjoy <sungmin4218@gmail.com>
2023-05-22 12:36:21 +0900

    ⭐ feat: 'S3 file 다중삭제'로직 구현 완료

--- src/main/java/com/adoptpet/server/commons/image/service/AwsS3Repository.java
+++ src/main/java/com/adoptpet/server/commons/image/service/AwsS3Repository.java
@@ -8,15 +8,14 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
 import com.amazonaws.services.s3.model.*;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
-import java.io.InputStream;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.amazonaws.services.s3.model.DeleteObjectsRequest.*;
 
 @Slf4j
 @Repository
@@ -45,7 +44,9 @@ public class AwsS3Repository {
     }
 
 
-    //== S3 이미지 파일 제거 ==//
+    /**
+    * S3 파일 단일 삭제
+    **/
     public String deleteFile(String keyName) {
         final AmazonS3Client aswS3Client = awsS3config.amazonS3Client();
         final String bucket = awsS3Properties.getBucket();
@@ -64,29 +65,49 @@ public class AwsS3Repository {
     }
 
     /**
-     * 다중 삭제
-    * 작성중
+     * S3 파일 다중 삭제
     **/
-    public void deleteFiles(String... keys){
+    public String deleteFiles(List<String> keyNames){
 
+        // AmazonS3Client 인스턴스를 생성하여 S3 클라이언트에 연결한다.
         final AmazonS3Client aswS3Client = awsS3config.amazonS3Client();
+
+        // 사용할 S3 버킷 이름을 가져온다.
         final String bucket = awsS3Properties.getBucket();
 
+        // 삭제할 객체 키(Key) 및 버전(KeyVersion)을 저장할 리스트를 생성한다.
+        List<KeyVersion> keys = new ArrayList<>();
+
+        // 전달받은 keyNames 리스트의 각 요소를 KeyVersion 객체로 생성하여 keys 리스트에 추가한다.
+        for(String keyName : keyNames){
+            keys.add(new KeyVersion(keyName));
+        }
+
+        // 삭제 요청 객체(DeleteObjectsRequest)를 생성한다.
+        // 해당 버킷(bucket)에서 keys 리스트에 있는 객체들을 삭제하도록 설정하고, quiet 모드를 비활성화(false)한다.
         DeleteObjectsRequest deleteRequest = new DeleteObjectsRequest(bucket)
                 .withKeys(keys)
                 .withQuiet(false);
 
+        // deleteObjects 메서드를 호출하여 객체를 삭제하고, 삭제 결과(DeleteObjectsResult)를 받는다.
         DeleteObjectsResult deleteResult = aswS3Client.deleteObjects(deleteRequest);
 
-        final int requestKeys = keys.length;
+        // 요청으로 전달된 키의 개수(requestKeys), 성공적으로 삭제된 객체 수(successfulDeletes), 남은 키 수(remainingKey)를 계산한다.
+        final int requestKeys = keyNames.size();
         final int successfulDeletes = deleteResult.getDeletedObjects().size();
         final int remainingKey = requestKeys - successfulDeletes;
 
-        String result;
-        if( remainingKey != 0 ){
-            result = String.format("AWS S3 - [%d] 개의 이미지가 삭제되지 않았습니다.", remainingKey);
-        }
-        result = "AWS S3 - Success delete all image file";
-    }
+        String result = "";
 
+        // 남은 키가 0이 아닌 경우, 삭제되지 않은 이미지 파일 개수를 메시지에 포함하여 결과를 설정한다.
+        if(remainingKey != 0 ){
+            result = String.format("AWS S3 - [%d]개의 이미지가 삭제되지 않았습니다.", remainingKey);
+        }
+
+        // 그렇지 않은 경우, 모든 이미지 파일이 성공적으로 삭제되었음을 나타내는 결과를 설정한다.
+        result = "AWS S3 - Success delete all image file";
+
+        // 최종 결과를 반환한다.
+        return result;
+    }
 }
