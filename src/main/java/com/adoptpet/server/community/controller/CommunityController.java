88774c000d61a6485191e13d5c2871043e958d24
keeptrynjoy <sungmin4218@gmail.com>
2023-05-25 13:56:51 +0900

    💅 style: 코드 정리

--- src/main/java/com/adoptpet/server/community/controller/CommunityController.java
+++ src/main/java/com/adoptpet/server/community/controller/CommunityController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.Map;
