5cab8adb1f53124a821d1573a3c38b3aba367277
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-28 21:10:21 +0900

    ⚙<fe0f> chore: 채팅방 목록 조회시 컬럼 일부 추가

--- src/main/java/com/adoptpet/server/adopt/mongo/MongoChatRepository.java
+++ src/main/java/com/adoptpet/server/adopt/mongo/MongoChatRepository.java
@@ -1,16 +1,16 @@
 package com.adoptpet.server.adopt.mongo;
 
 import com.adoptpet.server.adopt.domain.mongo.Chatting;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
-import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-
 import java.util.List;
 
 
 public interface MongoChatRepository extends MongoRepository<Chatting, String> {
 
     List<Chatting> findByChatRoomNo(Integer chatNo);
+
+    Page<Chatting> findByChatRoomNoOrderBySendDateDesc(Integer chatRoomNo, Pageable pageable);
 }
