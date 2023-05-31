53ae214613c1ac8f913fe94ff8d24b95470c50b0
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-31 19:56:47 +0900

    ⚙️ chore: Security URL mapping 규칙 일부 수정

--- src/main/java/com/adoptpet/server/commons/security/config/SecurityConfig.java
+++ src/main/java/com/adoptpet/server/commons/security/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                 .antMatchers("/user/**").hasAnyRole("MANAGER", "USER") // 회원 페이지는 회원(USER) 또는 관리자(MANAGER) 권한이 있어야 접근 가능
                 .antMatchers("/community/**").hasAnyRole("MANAGER", "USER") // GET 요청을 제외한 나머지 /community/** 요청은 권한 필요
                 .antMatchers("/admin/**").hasRole("MANAGER") // 관리자 페이지는 관리자(MANAGER) 권한이 있어야 접근 가능
+                .antMatchers("/chatroom/notification").permitAll()
                 .antMatchers("/chat/**").permitAll()
                 .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated() // 그 외의 모든 요청은 인증이 필요하다.
