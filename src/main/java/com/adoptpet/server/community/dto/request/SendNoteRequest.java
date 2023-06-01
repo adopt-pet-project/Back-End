3669fb18525469581b76e58073f770b9a0248085
keeptrynjoy <sungmin4218@gmail.com>
2023-06-01 13:48:20 +0900

    ♻️ refactor: [쪽지 전송] API 요청시 명칭 지정

--- src/main/java/com/adoptpet/server/community/dto/request/SendNoteRequest.java
+++ src/main/java/com/adoptpet/server/community/dto/request/SendNoteRequest.java
@@ -1,5 +1,6 @@
 package com.adoptpet.server.community.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,7 +12,9 @@ import javax.validation.constraints.NotBlank;
 public class SendNoteRequest {
 
     @Min(value = 0)
+    @JsonProperty("targetId")
     private final Integer receiverNo;
     @NotBlank
+    @JsonProperty("contents")
     private final String content;
 }
