2b24254fa4a8ae4aae05c9a4d3c9c8a21f54e01c
서민재 <seominjae@seominjaeui-Macmini.local>
2023-06-14 16:25:22 +0900

    🐞 fix: 채팅방 복귀시 update 메서드 채팅방 번호 누락문제 수정

--- src/test/java/com/adoptpet/server/BackEndApplicationTests.java
+++ src/test/java/com/adoptpet/server/BackEndApplicationTests.java
@@ -1,9 +1,6 @@
 package com.adoptpet.server;
 
-import com.adoptpet.server.user.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 class BackEndApplicationTests {
     public static void main(String[] args) {
-        SpringApplication.run(BackEndApplication.class, args);
+        SpringApplication.run(PetHubApplication.class, args);
     }
 
 //    @Autowired
