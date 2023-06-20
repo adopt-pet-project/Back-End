0bd7243c9764d31504763fd739c3e3847785bdbe
keeptrynjoy <sungmin4218@gmail.com>
2023-06-20 14:33:38 +0900

    🐞 fix: build 실패 원복

--- src/main/java/com/adoptpet/server/commons/security/config/SecurityConfig.java
+++ src/main/java/com/adoptpet/server/commons/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션관리 정책을 STATELESS(세션이 있으면 쓰지도 않고, 없으면 만들지도 않는다)
                 .and()
                 .authorizeRequests() // 요청에 대한 인증 설정
-                .antMatchers("/.well-known/pki-validation/**").permitAll()
+//                .antMatchers("/.well-known/pki-validation/**").permitAll()
                 .antMatchers("/token/**").permitAll() // 토큰 발급을 위한 경로는 모두 허용
                 .antMatchers(HttpMethod.GET, "/adopt/**").permitAll() //  /adopt/** GET 요청에 대해서는 권한 없이 접근 가능
                 .antMatchers(HttpMethod.GET, "/community/**").permitAll() // /community/** GET 요청에 대해서는 권한 없이 접근 가능
